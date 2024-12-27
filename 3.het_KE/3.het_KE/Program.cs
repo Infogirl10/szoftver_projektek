72a81a8bf8a592ab8405bbd2472fe6215caa90bd
Kusztor Éva <103362623+Infogirl10@users.noreply.github.com>
2024-12-27 17:05:21 +0100

    C# alapok példák ciklussal

---
+++ 3.het_KE/3.het_KE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace csharpalapok_elso_resz
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            //Az első 100 természetes szám összegének meghatározása
+           
+            //Szükség van egy változóra, amiben eltároljuk a számok összegét
+            int osszeg = 0;
+            //egy ciklus segítségével összeadjuk az egymás után számokat. Ehhez szükség van egy i változóra, illetve a számlálós for ciklusra. 
+            for (int i = 0; i <= 100; i++)
+            {
+                //A ciklusmag használjuk ehhez a osszeg változót amihez mindig hozzádjuk az i értékét.
+                osszeg = osszeg+i;
+               
+                Console.WriteLine(osszeg+i);
+            }
+            //Összeg kiiratásához a Console.WriteLine-t kell használni, illetve az osszeg nevu változót.
+            Console.WriteLine("Az első 100 természetes szám összege: {0}", osszeg);
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
+
---
+++ 3.het_KE/3.het_KE_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.het_KE_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Átlagszámítás
+            int szam = 1;
+            int db = 0;
+            int osszeg = 0;
+            Console.WriteLine("A számításhoz meg kell adni a végén a 0-t.");
+            //Addig kérjük be számokat amíg nem 0-t adunk meg
+            while (szam != 0)
+            {
+               
+                Console.Write("Adj meg egy számot: ");
+                szam = int.Parse(Console.ReadLine());
+                db++;
+                if (szam == 0)
+                {
+                   
+                }
+                osszeg = osszeg+szam;
+            }
+            //Átlagszámítás: bekért számok összege osztva a bekért számok darabszámával.
+            double atlag = osszeg / db;
+            Console.WriteLine("A számok átlaga: {0}", atlag);
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
+
